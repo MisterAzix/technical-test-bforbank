f2b4a0f8ec763de0149aea0329973a58c1dc563f
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-06-29 18:05:59 +0200

    chore: init project 🎉

---
+++ src/main/java/com/bforbank/moneypot/TechnicalTestBforbankApplication.java
@@ -0,0 +1,13 @@
+package com.bforbank.moneypot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestBforbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestBforbankApplication.class, args);
+	}
+
+}
