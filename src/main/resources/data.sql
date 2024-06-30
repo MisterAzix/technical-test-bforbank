7356c823cb9a4221265b0d4797f88828d006fe40
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-07-01 00:31:32 +0200

    fix: data.sql and schema.sql to prevent duplicate data 🩹

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO moneypots (client_id, amount, number_of_transactions, created_at, updated_at) VALUES (1, 15.00, 3, '2020-01-01', '2020-01-01'), (2, 20.00, 2, '2020-01-01', '2020-01-01');
+INSERT INTO moneypots (client_id, amount, number_of_transactions, created_at, updated_at)
+VALUES
+    (1, 15.00, 3, '2020-01-01', '2020-01-01'),
+    (2, 20.00, 2, '2020-01-01', '2020-01-01')
+    ON CONFLICT (client_id) DO NOTHING;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS moneypots (
     id SERIAL PRIMARY KEY,
-    client_id BIGINT NOT NULL,
+    client_id BIGINT NOT NULL UNIQUE,
     amount DECIMAL(10, 2) NOT NULL,
     number_of_transactions INT NOT NULL,
     created_at TIMESTAMP NOT NULL,
